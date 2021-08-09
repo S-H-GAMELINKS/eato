2535795fac5921bbca9dea4a0e324ecb84a4e255
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-08-09 22:26:09 +0900

    Create Restaurant model

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=255)
+    tel_number = models.CharField(max_length=20)
+
