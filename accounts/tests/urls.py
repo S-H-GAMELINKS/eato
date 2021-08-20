e19917d2fb5f2d6508968d76b55f012c64ae9730
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-08-20 18:28:47 +0900

    Add test form mypage_update

--- accounts/tests/urls.py
+++ accounts/tests/urls.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from accounts.views import mypage
+from accounts.views import mypage, mypage_edit, mypage_update
 
 class AccountUrlTestCase(TestCase):
     fixtures = ['accounts_url_testdata.json']
@@ -11,3 +11,11 @@ class AccountUrlTestCase(TestCase):
     def test_accounts_mypage_url(self):
         url = reverse('accounts:mypage')
         self.assertEqual(resolve(url).func, mypage)
+
+    def test_accounts_mypage_edit_url(self):
+        url = reverse('accounts:mypage_edit')
+        self.assertEqual(resolve(url).func, mypage_edit)
+
+    def test_accounts_mypage_update_url(self):
+        url = reverse('accounts:mypage_update')
+        self.assertEqual(resolve(url).func, mypage_update)
