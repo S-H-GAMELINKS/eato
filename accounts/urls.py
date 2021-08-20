da1f36a6d587fca44a4b9a9e174f4eeabdd93bf4
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-08-20 18:29:06 +0900

    Add mypage_edit & mypage_update url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('', views.mypage, name='mypage'),
+    path('_edit', views.mypage_edit, name='mypage_edit'),
+    path('_update', views.mypage_update, name='mypage_update'),
 ]
