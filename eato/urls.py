3ac7de20b51beeb3d0c54dd2e6ad6c242401a310
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-08-21 17:59:28 +0900

    Add lp url

--- eato/urls.py
+++ eato/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from eato.views import lp
 
 urlpatterns = [
+    path('', lp, name='lp'),
     path('restaurants/', include('restaurants.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
