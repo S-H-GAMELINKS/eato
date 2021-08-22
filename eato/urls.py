c46e73ab2af26077b5cfcfbbaca799b8ad208233
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-08-22 22:23:16 +0900

    Fix url pattern for accounts

--- eato/urls.py
+++ eato/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path('restaurants/', include('restaurants.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('accounts/mypage', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
