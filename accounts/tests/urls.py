e0bdd626777774d2536205e54cd319d0ae4cef0a
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-08-22 22:07:49 +0900

    Add test for accounts:detail

--- accounts/tests/urls.py
+++ accounts/tests/urls.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from accounts.views import mypage, mypage_edit, mypage_update
+from django.contrib.auth.models import User
+from accounts.views import mypage, mypage_edit, mypage_update, detail
 
 class AccountUrlTestCase(TestCase):
     fixtures = ['accounts_url_testdata.json']
 
     def setUp(self):
         super(AccountUrlTestCase, self).setUp()
+        self.user = User.objects.get(pk=1)
 
     def test_accounts_mypage_url(self):
         url = reverse('accounts:mypage')
@@ -19,3 +21,7 @@ class AccountUrlTestCase(TestCase):
     def test_accounts_mypage_update_url(self):
         url = reverse('accounts:mypage_update')
         self.assertEqual(resolve(url).func, mypage_update)
+
+    def test_accounts_mypage_detail_url(self):
+        url = reverse('accounts:detail', kwargs={'user_id': self.user.id})
+        self.assertEqual(resolve(url).func, detail)
