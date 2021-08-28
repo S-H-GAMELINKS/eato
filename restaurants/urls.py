b2ce6aafdd9d5f2518902c23d629e09001217f79
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-08-28 19:27:14 +0900

    Add restaurants review like url

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:restaurant_id>/', views.detail, name='detail'),
     path('<int:restaurant_id>/favorites', views.favorites, name='favorites'),
     path('<int:restaurant_id>/reviews', views.reviews, name='reviews'),
+    path('<int:restaurant_id>/reviews/<int:review_id>/likes', views.likes, name='likes')
 ]
