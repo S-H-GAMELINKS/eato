74f0c3ec4c1d1b18bc81674531aa5dbfe52e6035
hotate1000 <ooo.336.sss@gmail.com>
2021-09-05 23:49:12 +0900

    プライバシーページ、利用規約ページ追加

--- eato/urls.py
+++ eato/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from eato.views import lp
+from eato.views import lp,TermsClass,PrivacyClass
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -33,5 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('accounts.urls')),
+    path('privacy/', PrivacyClass.as_view()),
+    path('terms/', TermsClass.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- eato/views.py
+++ eato/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from restaurants.models import Restaurant, Review
+from django .views.generic import TemplateView
 
 def lp(request):
     restaurant_list = Restaurant.objects.order_by('?')[:9]
     review_list = Review.objects.select_related('restaurant').order_by('?')[:9]
     return render(request, 'lp.html', {'restaurant_list': restaurant_list, 'review_list': review_list})
+
+class PrivacyClass(TemplateView):
+    template_name = 'privacy.html';
+
+class TermsClass(TemplateView):
+    template_name = 'terms.html';
