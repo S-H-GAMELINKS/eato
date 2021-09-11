4d792c65ed0325922c078a236e45a420e2896d38
hotate1000 <ooo.336.sss@gmail.com>
2021-09-11 21:43:53 +0900

    クラス部分を関数に変更

--- eato/urls.py
+++ eato/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from eato.views import lp,TermsClass,PrivacyClass
+from eato.views import lp,privacy,terms
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -33,7 +33,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('accounts.urls')),
-    path('privacy/', PrivacyClass.as_view()),
-    path('terms/', TermsClass.as_view()),
+    path('privacy', privacy, name='privacy'),
+    path('terms', terms, name='terms')
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
